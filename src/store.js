478a9bb8594ae9e4953d41e4990ff77045e6d9b0
Your Name <you@example.com>
2019-12-12 20:35:49 +0100

    Prvi commit

---
+++ src/store.js
@@ -0,0 +1,10 @@
+export default {
+    authenticated: false,
+    userEmail: 'fake@email.com',
+    userName: 'Neki user',
+    searchTerm: '',
+    cards: [
+      { user:'ivan', title: 'jedan', time: 'an hour ago', url: '/images/unsplash.jpg' },
+      { user: 'ana',title: 'dva', time: '2 days ago', url: '/images/unsplash.jpg' },
+    ]        
+}
